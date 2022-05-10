b63e216b93976c7e117df9a5420b95bb775b1247
Deepak <deepakdeepakpandey25@gmail.com>
2022-05-10 17:17:41 +0530

    init

--- Data/rename_files.py
+++ Data/rename_files.py
@@ -1,7 +1,7 @@
 # file to rename files in a directory
 import os
-for i in range(41,51):
-    old_name = r"Data/Test_data/Image_"+str(i)+".jpg"
-    new_name = r"Data/Test_data/Image_"+str(i-40)+".jpg"
+for i in range(1,11):
+    old_name = r"data/rotten_banana_fruit/Image_"+str(i)+".jpg"
+    new_name = r"data/rotten_banana_fruit/Image_"+str(i+10)+".jpg"
     os.rename(old_name, new_name)
 
